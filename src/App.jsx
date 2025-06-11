825f475f26d39bdc0f810ecd158556ce077e07a3
abhishek sharma <abhisheksharma2920@gmail.com>
2025-06-11 17:18:38 +0530

    minor change

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,23 @@
 import { useEffect, useState } from "react";
-import "./App.css";
+
 function App() {
-  const [inputData, setInputData] = useState("");
   const [todoData, setTodoData] = useState([]);
+  const [input, setInput] = useState("");
   const [editableTodoMsg, setEditableTodoMsg] = useState("");
   const [editableTodoId, setEditableTodoId] = useState(null);
   const [error, setError] = useState("");
 
   const handleTodoList = () => {
     setError("");
-    if (inputData.trim() !== "") {
-      const newTodo = { id: Date.now(), todoMsg: inputData };
+    if (input.trim() !== "") {
+      const newTodo = { id: Date.now(), todoMsg: input };
       const updatedTodos = [...todoData, newTodo];
       setTodoData(updatedTodos);
       sessionStorage.setItem("todoData", JSON.stringify(updatedTodos));
-      setInputData("");
+      setInput("");
     } else {
       setError("Todo is required");
+      setTimeout(() => setError(""), 2000);
     }
   };
 
@@ -28,9 +29,9 @@ function App() {
   }, []);
 
   const handleRemoveTodo = (id) => {
-    const updatedTodos = todoData.filter(todo => todo.id !== id);
+    const updatedTodos = todoData.filter((todo) => todo.id !== id);
     setTodoData(updatedTodos);
-    localStorage.setItem("todoData", JSON.stringify(updatedTodos));
+    sessionStorage.setItem("todoData", JSON.stringify(updatedTodos));
   };
 
   const handleEditTodo = (id) => {
@@ -40,11 +41,11 @@ function App() {
   };
 
   const handleSaveTodo = (id) => {
-    const updatedTodos = todoData.map(todo =>
+    const updatedTodos = todoData.map((todo) =>
       todo.id === id ? { ...todo, todoMsg: editableTodoMsg } : todo
     );
     setTodoData(updatedTodos);
-    localStorage.setItem("todoData", JSON.stringify(updatedTodos)); // Update localStorage
+    sessionStorage.setItem("todoData", JSON.stringify(updatedTodos)); // Update localStorage
     setEditableTodoId(null);
   };
 
@@ -60,12 +61,12 @@ function App() {
               className="px-3 py-2 w-full rounded-lg shadow-2xl text-gray-700 font-medium focus:shadow-2xl focus:outline-none"
               type="text"
               placeholder="Write Notes..."
-              value={inputData}
-              onChange={(e) => setInputData(e.target.value)}
+              value={input}
+              onChange={(e) => setInput(e.target.value)}
             />
             <button
               onClick={handleTodoList}
-              className="px-5 py-2 text-gray-600 font-semibold bg-blue-300 rounded-lg"
+              className="px-5 py-2 text-white font-semibold bg-blue-500 rounded-lg transition duration-100 ease-in hover:bg-[#202020]"
             >
               Add
             </button>
@@ -75,19 +76,19 @@ function App() {
             {todoData.map((todo) => (
               <div
                 key={todo.id}
-                className="w-[250px] h-[300px] flex flex-col gap-2 pt-4 pb-2 px-2 bg-gray-600 rounded-md my-2 max-[500px]:w-[320px] max-[500px]:h-[400px]"
+                className="w-[250px] h-[300px] flex flex-col gap-2 pt-4 pb-2 px-2 bg-gray-600 rounded-md my-2 max-[600px]:w-[320px] max-[600px]:h-[400px]"
               >
                 {editableTodoId === todo.id ? (
                   <>
                     <textarea
-                      className="px-3 py-2 w-full h-full resize-none rounded-lg text-gray-800 bg-gray-200 font-semibold shadow-2xl border-2 focus:outline-none"
+                      className="save px-3 py-2 w-full h-full resize-none rounded-lg text-gray-800 bg-green-100 font-semibold shadow-2xl border-2 focus:outline-none"
                       type="text"
                       value={editableTodoMsg}
                       onChange={(e) => setEditableTodoMsg(e.target.value)}
                     />
                     <button
                       onClick={() => handleSaveTodo(todo.id)}
-                      className="px-5 py-2 text-black font-semibold bg-white rounded-lg shadow-xl transition duration-200 ease-in hover:bg-green-500 hover:text-white"
+                      className="px-5 py-2 text-black font-semibold bg-green-400 rounded-lg shadow-xl transition duration-200 ease-in hover:bg-green-600 hover:text-white"
                     >
                       Save
                     </button>
@@ -95,14 +96,14 @@ function App() {
                 ) : (
                   <>
                     <textarea
-                      className="px-3 py-2 w-full h-full resize-none rounded-lg text-white bg-gray-900 font-semibold focus:outline-none"
+                      className="edit px-3 py-2 w-full h-full resize-none rounded-lg text-white bg-gray-900 font-semibold focus:outline-none"
                       type="text"
                       readOnly
                       value={todo.todoMsg}
                     />
                     <button
                       onClick={() => handleEditTodo(todo.id)}
-                      className="px-5 py-2 text-black font-semibold bg-white transition duration-200 ease-in hover:bg-yellow-300 hover:text-gray-700 rounded-lg shadow-xl"
+                      className="px-5 py-2 text-black font-semibold bg-yellow-300 transition duration-200 ease-in rounded-lg shadow-xl hover:bg-yellow-400"
                     >
                       Edit
                     </button>
@@ -110,7 +111,7 @@ function App() {
                 )}
                 <button
                   onClick={() => handleRemoveTodo(todo.id)}
-                  className="px-5 py-2 text-black font-semibold bg-white transition duration-200 ease-in hover:bg-red-500 hover:text-white rounded-lg shadow-xl"
+                  className="px-5 py-2 text-white font-semibold bg-red-400 transition duration-200 ease-in rounded-lg shadow-xl hover:bg-red-500"
                 >
                   Delete
                 </button>
